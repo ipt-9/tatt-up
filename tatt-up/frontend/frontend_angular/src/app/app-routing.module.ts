218a87300eddc407f153f941d48d29c7fcf448a9
Mathusiga Nithivathanan <mathusiga_nithivath@sluz.ch>
2024-02-29 16:50:46 +0100

    added routing layout

--- tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
+++ tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent} from "./layout/layout.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'layout', component: LayoutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
