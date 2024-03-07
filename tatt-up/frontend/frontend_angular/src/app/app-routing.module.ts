93b2b7d38a95789d3cfe570bd59a72bc7dce47f5
Aline Schwegler <aline_schwegler@sluz.ch>
2024-03-07 14:45:54 +0100

    finished footer

--- tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
+++ tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent} from "./layout/layout.component";
+import { FooterComponent} from "./footer/footer.component";
 
 
 const routes: Routes = [
   {path: 'layout', component: LayoutComponent},
-
+  {path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
