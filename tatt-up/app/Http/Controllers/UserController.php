cd9f33d643343f2e03904e7a35a70e18654b2b35
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-07 15:59:11 +0100

    Added user components

--- tatt-up/app/Http/Controllers/UserController.php
+++ tatt-up/app/Http/Controllers/UserController.php
@@ -2,8 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use function Laravel\Prompts\password;
+
 class UserController extends Controller
 {
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|email|unique:users|max:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:user,tattooer,designer',
+        ]);
+        //New user instance
+        $user = new User();
+        $user->username = $validatedData['username'];
+        $user->email = $validatedData['email'];
+        $user->password = bcrypt($validatedData['password']);
+        $user->role = $validatedData['role'];
 
+        $user->save();
+
+        return UserResource::make($user);
+    }
 
 }
