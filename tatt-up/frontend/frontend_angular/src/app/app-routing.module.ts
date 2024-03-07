f15c851f58ee6ed6f89a606baa1826d9fc1ae0cb
Aline Schwegler <aline_schwegler@sluz.ch>
2024-03-07 17:24:26 +0100

    searchbar added

--- tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
+++ tatt-up/frontend/frontend_angular/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent} from "./layout/layout.component";
 import { FooterComponent} from "./footer/footer.component";
 import { NavigationComponent} from "./navigation/navigation.component";
-
+import { SearchBarComponent} from "./search-bar/search-bar.component";
 
 
 const routes: Routes = [
   {path: 'layout', component: LayoutComponent},
   {path: 'footer', component: FooterComponent},
-  {path: 'navigation', component: NavigationComponent}
+  {path: 'navigation', component: NavigationComponent},
+  {path: 'search', component: SearchBarComponent}
 ];
 
 @NgModule({
