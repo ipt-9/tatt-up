3530cf9a273b819912543becc938af36d7986f70
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-14 17:01:18 +0100

    Added Seeder #2

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Type\Integer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' =>fake(),
+            'likes'=>fake()->numberBetween(0, 10000),
+            'caption'=>fake()->words,
+            'file_path'=>fake()->filePath(),
+        ];
+    }
+}
