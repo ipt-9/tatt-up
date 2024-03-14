d31fc39386307fc3925c966f920d6eaa26eaf6b1
Sara Ramirez <102947286+saractrl@users.noreply.github.com>
2024-03-14 14:44:46 +0100

    Backend sc (#51)
    * Added user components
    
    * User interactions
    
    * Trying to migrate
    
    * Added migrations
    
    ---------
    
    Co-authored-by: Sara Ramirez <sara_cabreraramirez@sluz.ch>

--- tatt-up/app/Http/Controllers/UserController.php
+++ tatt-up/app/Http/Controllers/UserController.php
@@ -2,8 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use function Laravel\Prompts\password;
+
 class UserController extends Controller
 {
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|email|unique:users|max:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|in:user,tattooer,designer',
+        ]);
+        //New user instance
+        $user = new User();
+        $user->username = $validatedData['username'];
+        $user->email = $validatedData['email'];
+        $user->password = bcrypt($validatedData['password']);
+        $user->role = $validatedData['role'];
 
+        $user->save();
+
+        return UserResource::make($user);
+    }
 
 }
--- tatt-up/database/factories/UserFactory.php
+++ tatt-up/database/factories/UserFactory.php
@@ -23,11 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $roles = ['user', 'tattooer', 'designer'];
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role' => $this->faker->randomElement($roles),
             'remember_token' => Str::random(10),
         ];
     }
