9d9c2ae3e5fe3a467a8a8408c8960ae5b59a8441
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-14 14:24:49 +0100

    Trying to migrate

---
+++ tatt-up/config/ssh.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'server1' => [
+        'host' => '195.65.36.215',
+        'port' => 22,
+        'username' => 'tatt-up',
+        'password' => 'seH57$b0whJyEay#',
+
+    ],
+
+];
