29969adf25c56a3f73fa53a791b8f445254d6b7d
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-17 16:37:27 +0100

    Changed UserFactory (Attempting to seed)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,8 @@ class UserFactory extends Factory
     {
         $roles = ['user', 'tattooer', 'designer'];
         return [
-            'username' => fake()->userName(),
-            'email' => fake()->unique()->safeEmail(),
+            'username' => $this->faker->userName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'role' => $this->faker->randomElement($roles),
