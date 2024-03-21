1705f054e8e97aba152c21b3b9e180885154b3fc
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-21 13:12:54 +0100

    Added routes api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'errors' => ['general' => 'E-Mail or Passwort wrong.']
+            ], 422);
+
+        }
+        return ['token' => $request->user()->createToken('auth_token')->plainTextToken];
+
+    }
+    public function checkAuth(Request $request)
+    {
+        return UserResource::make($request->user());
+    }
     public function logout(Request $request): JsonResponse
     {
-        $user = $request->user();
-        $user->tokens->each(function ($token) {
-            $token->delete();
-        });
+        $request->user()->tokens()->delete();
 
         return response()->json(['message' => 'Logged out successfully']);
     }
+
 }
