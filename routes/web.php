59826bb91af5e2a1cd1c53287c212714d6fb3e3d
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-24 20:46:28 +0100

    Changed composer.json

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/users/create', 'UserController@create')->name('users.create');
+Route::get('/users/create', 'UserController@create');
 Route::post('/users', 'UserController@store');
 
 require __DIR__.'/auth.php';
