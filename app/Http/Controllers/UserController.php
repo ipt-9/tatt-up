e9bf559f774002f83c85f29a0578cd017a1c73fd
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-04-18 16:46:45 +0200

    Changed composer.json

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,24 +5,30 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use function Laravel\Prompts\password;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
+        $userData = json_decode($request->getContent(), true);
+
+        $validator = Validator::make($userData, [
             'username' => 'required|string|max:255|unique:users',
             'email' => 'required|email|unique:users',
             'password' => 'required|string|min:8',
             'role' => 'required|in:user,tattooer,designer',
         ]);
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
         //New user instance
         $user = new User();
-        $user->username = $validatedData['username'];
-        $user->email = $validatedData['email'];
-        $user->password = bcrypt($validatedData['password']);
-        $user->role = $validatedData['role'];
+        $user->username = $userData['username'];
+        $user->email = $userData['email'];
+        $user->password = bcrypt($userData['password']);
+        $user->role = $userData['role'];
         $user->save();
 
         //return UserResource::make($user);
